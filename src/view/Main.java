fea7cce5d8571b8b20c0d635f39f3498eceb5f5a
Zaphyk <zaphyk@live.com>
2017-10-21 23:49:44 -0300

    added 2 different ui types

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
+        primaryStage.setTitle("Project Hedra Launcher");
+        primaryStage.setScene(new Scene(root, 925, 500));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
